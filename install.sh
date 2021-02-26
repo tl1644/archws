7dd438c66d08356ab5082bb7cfb256bba89b7e3d
tl1644 <67839530+tl1644@users.noreply.github.com>
2021-02-26 17:25:42 +0100

    Basic install steps added

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-test
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda1
 
+mount /dev/sda2 /mnt
+swapon /dev/sda1
+
+pacstrap /mnt base linux linux-firmware
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
