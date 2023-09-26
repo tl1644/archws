067e17d62d38c8aee911d39c5d5e8b1a8c2d67b3
tl1644 <tl1644@gmail.com>
2023-09-26 08:38:39 +0200

    bugfix

--- base_install.sh
+++ base_install.sh
@@ -5,5 +5,6 @@ pacstrap -K /mnt base linux linux-firmware reflector rsync btrfs-progs
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp -r /root/archws /mnt/root
+cp /root/cryptpart.keyfile /mnt/root
 
 arch-chroot /mnt
--- make_bootable.sh
+++ make_bootable.sh
@@ -3,6 +3,5 @@
 mkinitcpio -p linux
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
-grub-mkconfig -o /boot/grub/grub.cfg
-
 cp files/grub /etc/default/
+grub-mkconfig -o /boot/grub/grub.cfg
