e1450dd008ab177866c8335e103ae9f6872cee96
tl1644 <tl1644@gmail.com>
2023-09-26 09:46:55 +0200

    bugfix

--- make_bootable.sh
+++ make_bootable.sh
@@ -2,6 +2,6 @@
 
 mkinitcpio -p linux
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
 cp files/grub /etc/default/
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=OS
 grub-mkconfig -o /boot/grub/grub.cfg
